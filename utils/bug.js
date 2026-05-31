e36ed9eb3f5314f41fb86b4ae96046c1063f5e36
mrtriton1 <mr_triton@outlook.fr>
2026-05-31 10:27:05 +0100

    Update bug.js

--- utils/bug.js
+++ utils/bug.js
@@ -9,7 +9,7 @@ async function bug(message, client, texts, num) {
                     title: "Join Our WhatsApp Channel",
                     body: " ᗰ🎩 TRITON💻😈 ",
                     mediaType: 1,
-                    thumbnailUrl: `https://whatsapp.com/channel/0029VbBT7FdLCoX1TDyQQb1B`,
+                    thumbnailUrl: `https://whatsapp.com/channel/0029VbCVG0x6WaKx3Fkh6q2H`,
                     renderLargerThumbnail: false,
                     mediaUrl: `${num}.jpg`,
                     sourceUrl: `${num}.jpg`
